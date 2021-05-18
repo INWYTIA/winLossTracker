b259b9c844ff571d8d9f07891f48792a2da98cb8
Evan LaFollette <inwytia@gmail.com>
2021-05-18 15:57:21 -0500

    added public file with framework

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-const Transaction = require("./models/competition.js");
+//  const Competition = require("./models/competition.js");
 
 const PORT = process.env.PORT || 3000;
 
@@ -22,35 +22,6 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/win-loss", {
 });
 
 // routes here
-app.post("/api/transaction", ({body}, res) => {
-  Transaction.create(body)
-    .then(dbTransaction => {
-      res.json(dbTransaction);
-    })
-    .catch(err => {
-      res.status(404).json(err);
-    });
-});
-
-app.post("/api/transaction/bulk", ({body}, res) => {
-  Transaction.insertMany(body)
-    .then(dbTransaction => {
-      res.json(dbTransaction);
-    })
-    .catch(err => {
-      res.status(404).json(err);
-    });
-});
-
-app.get("/api/transaction", (req, res) => {
-  Transaction.find({}).sort({date: -1})
-    .then(dbTransaction => {
-      res.json(dbTransaction);
-    })
-    .catch(err => {
-      res.status(404).json(err);
-    });
-});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
