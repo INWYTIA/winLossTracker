e959fa5006d127f3aa148225b7f7eec9fd3f9bf8
Evan LaFollette <inwytia@gmail.com>
2021-06-15 17:07:36 -0500

    studying core concepts

--- public/index.js
+++ public/index.js
@@ -1,35 +1,41 @@
-$(function(){
-  $("#c-submit").on("click", function(event) {
-
-    let newComp = {
-      name: $("#c-name").val().trim()
-    };
-
-    $.ajax("/api/competition", {
-      method: "POST",
-      data: JSON.stringify(newComp),
-      headers: {
-        Accept: "application/json, text/plain, */*",
-        "Content-Type": "application/json"
-      }
-    })//.then(function() {
-    //  location.reload();
-    //});
+angular.module('primary', [])
+  .controller('PrimaryController', function() {
+    
   })
 
-  $(".compCho").on("click", function(event) {
 
-    let compCho = {
-      id: $(this).attr("data-id")
-    };
+// $(function () {
+//   $("#c-submit").on("click", function (event) {
 
-    $.ajax("/api/competition/detail", {
-      method: "GET",
-      data: compCho,
-      headers: {
-        Accept: "application/json, text/plain, */*",
-        "Content-Type": "application/json"
-      }
-    });
-  })
-});
+//     let newComp = {
+//       name: $("#c-name").val().trim()
+//     };
+
+//     $.ajax("/api/competition", {
+//       method: "POST",
+//       data: JSON.stringify(newComp),
+//       headers: {
+//         Accept: "application/json, text/plain, */*",
+//         "Content-Type": "application/json"
+//       }
+//     }).then(function () {
+//       location.reload();
+//     });
+//   })
+
+//   $(".compCho").on("click", function (event) {
+
+//     let compCho = {
+//       id: $(this).attr("data-id")
+//     };
+
+//     $.ajax("/api/competition/detail", {
+//       method: "GET",
+//       data: compCho,
+//       headers: {
+//         Accept: "application/json, text/plain, */*",
+//         "Content-Type": "application/json"
+//       }
+//     });
+//   })
+// });
